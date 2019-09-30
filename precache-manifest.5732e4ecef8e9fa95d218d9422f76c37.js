afaa695554a5733b7e72a533e626ed5d0bb844c0
YuryIT <kumirovyura.13@mail.ru>
2019-09-30 19:15:30 +0400

    Updates

---
+++ precache-manifest.5732e4ecef8e9fa95d218d9422f76c37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ed646066606c8c626cb9400f51f1a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d90edc316f449a4ff15",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "171c92cc86fadce0f89d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "1d90edc316f449a4ff15",
+    "url": "./static/js/2.385a066f.chunk.js"
+  },
+  {
+    "revision": "171c92cc86fadce0f89d",
+    "url": "./static/js/main.39a8f4cb.chunk.js"
+  },
+  {
+    "revision": "8757863103e7b68d715c",
+    "url": "./static/js/runtime-main.e1dbd278.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
